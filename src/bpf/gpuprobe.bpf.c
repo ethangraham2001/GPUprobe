2ac7c50026912c2dde443f25d42f0c290b44bca9
Ethan Graham <ethan.w.s.graham@gmail.com>
2024-10-22 23:09:52 +0200

    successfully able to trace cuda malloc calls

--- src/bpf/gpuprobe.bpf.c
+++ src/bpf/gpuprobe.bpf.c
@@ -17,12 +17,37 @@ struct {
 	__uint(max_entries, 1);
 } cuda_malloc_failures SEC(".maps");
 
+/**
+ * cuda memory allocations that have been launched
+ */
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
 	__type(key, u64);
 	__type(value, size_t);
 	__uint(max_entries, 10240);
-} alive_allocs SEC(".maps");
+} launched_allocs SEC(".maps");
+
+/**
+ * cuda memory allocations that have succeeded
+ */
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__type(key, u64);
+	__type(value, size_t);
+	__uint(max_entries, 10240);
+} successful_allocs SEC(".maps");
+
+/**
+ * The passed `dev_ptr` parameter can only be read from the inital uprobe. We
+ * store it before execution so that we can read the virtual address of the
+ * device in the uretprobe
+ */
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__type(key, u32);
+	__type(value, void**);
+	__uint(max_entries, 10240);
+} pid_to_dev_ptr SEC(".maps");
 
 void **ptr_addr;
 
@@ -31,6 +56,7 @@ int trace_cuda_malloc(struct pt_regs *ctx)
 {
 	u32 key0 = 0;
 	u64 *num_mallocs;
+	u32 pid;
 	void **dev_ptr;
 	size_t size;
 
@@ -41,10 +67,10 @@ int trace_cuda_malloc(struct pt_regs *ctx)
 
 	dev_ptr = (void**)PT_REGS_PARM1(ctx);
 	size = (size_t)PT_REGS_PARM2(ctx);
+	bpf_map_update_elem(&launched_allocs, &dev_ptr, &size, 0);
 
-	if (bpf_map_update_elem(&alive_allocs, &dev_ptr, &size, 0b0)) {
-		bpf_printk("unable to update hash map");
-	}
+	pid = (u32)bpf_get_current_pid_tgid();
+	bpf_map_update_elem(&pid_to_dev_ptr, &pid, &dev_ptr, 0);
 
 	return 0;
 }
@@ -52,21 +78,38 @@ int trace_cuda_malloc(struct pt_regs *ctx)
 SEC("uretprobe/cudaMalloc")
 int trace_cuda_malloc_ret(struct pt_regs *ctx)
 {
-	u32 key0 = 0;
-	u64 *num_mallocs;
+	int cuda_malloc_ret;
+	u32 pid, key0 = 0;
+	size_t *size, *num_failures;
+	void *alloc_ptr;
 	void **dev_ptr;
-	void *allocated_ptr;
-	size_t size;
+	void ***map_ptr;
 
-	if (bpf_probe_read_user(&allocated_ptr, sizeof(allocated_ptr), \
-				ptr_addr)) {
-		bpf_printk("unable to read from dev_ptr");
-		return 0;
+	cuda_malloc_ret = (int)PT_REGS_RC(ctx);
+	if (cuda_malloc_ret) {
+		num_failures = bpf_map_lookup_elem(&cuda_malloc_failures, &key0);
+		if (num_failures)
+			__sync_fetch_and_add(num_failures, 1);
+	}
+	
+	pid = (u32)bpf_get_current_pid_tgid();
+	map_ptr = bpf_map_lookup_elem(&pid_to_dev_ptr, &pid);
+
+	if (!map_ptr)
+		return -1;
+
+	dev_ptr = *map_ptr;
+	if (bpf_probe_read_user(&alloc_ptr, sizeof(alloc_ptr), dev_ptr)) {
+		return -1;
 	}
 
-	if (allocated_ptr) {
-		bpf_printk("allocated_ptr=%p", allocated_ptr);
+	size = bpf_map_lookup_elem(&launched_allocs, &dev_ptr);
+	if (!size) {
+		return -1;
 	}
+
+	bpf_printk("host_ptr = 0x%p, device_ptr = 0x%p", dev_ptr, alloc_ptr);
+	bpf_map_update_elem(&successful_allocs, &alloc_ptr, size, 0);
 
 	return 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,10 @@ use gpuprobe::*;
 fn main() {
     let skel_builder = GpuprobeSkelBuilder::default();
     let mut open_object = MaybeUninit::uninit();
-    let open_skel = skel_builder.open(&mut open_object)
+    let open_skel = skel_builder
+        .open(&mut open_object)
         .expect("unable to open skeleton");
-    let skel = open_skel.load()
-        .expect("unable to load skeleton");
+    let skel = open_skel.load().expect("unable to load skeleton");
 
     let _uprobe_link = skel
         .progs
@@ -46,10 +46,9 @@ fn main() {
         )
         .expect("unable to attach cuda uretprobe");
 
-
     let key0 = vec![0u8; 4];
     loop {
-        let mallocs = skel
+        let num_cuda_malloc_calls = skel
             .maps
             .num_cuda_malloc_calls
             .lookup(&key0, MapFlags::ANY)
@@ -64,23 +63,29 @@ fn main() {
             })
             .expect("unable to perform map lookup");
 
+        println!("total number of `cudaMalloc` calls: {}", num_cuda_malloc_calls);
+        println!("outstanding allocations");
 
-        skel.maps.alive_allocs.keys().for_each(|addr| {
+        skel.maps.successful_allocs.keys().for_each(|addr| {
             let addr_key: [u8; 8] = addr.try_into().expect("unexpected key size");
-            let outstanding_allocs = skel.maps.alive_allocs
+            let outstanding_allocs = skel
+                .maps
+                .successful_allocs
                 .lookup(&addr_key, MapFlags::ANY)
                 .expect("no value found");
             match outstanding_allocs {
                 Some(count) => {
                     let count: [u8; 8] = count.try_into().expect("unable to convert result");
-                    println!("0x{:8x} -> {} bytes", u64::from_ne_bytes(addr_key),
-                        u64::from_ne_bytes(count));
-                },
+                    println!(
+                        "0x{:8x} -> {} bytes",
+                        u64::from_ne_bytes(addr_key),
+                        u64::from_ne_bytes(count)
+                    );
+                }
                 None => todo!(),
             }
         });
 
-        println!("num cudaMallocs: {}", mallocs);
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
